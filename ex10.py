927f0594ec621985216d360902ac7f9349fd5443
cspears2002 <cspears2002@yahoo.com>
2017-06-18 20:24:25 -0700

    Finished exercises 9 - 10

---
+++ ex10.py
@@ -0,0 +1,30 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
+
+fat_dog = '''
+I\'ll do a list:
+\t* Dog food
+\t* Steak
+\t* Grass
+'''
+
+print(fat_dog)
+
+print("Let's make some noise!")
+print("\a")
+print("\a")
+print("Let's do a formfeed! \fForm feed!")
+print("ASCII vertical tab. \vVertical tab.\vVertical tab.")
---
+++ ex9.py
@@ -0,0 +1,21 @@
+# Here's some new strange stuff, remember type it exactly.
+
+# Set days to a long string
+days = "Mon Tue Wed Thu Fri Sat Sun"
+
+# Set months to a long string with carriage returns
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+# Print the days on one line
+print("Here are the days: ", days)
+
+# Print the months across multiple lines
+print("Here are the months: ", months)
+
+# Print the entire strong across several lines
+print("""
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+""")
