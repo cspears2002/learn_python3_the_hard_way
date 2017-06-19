be9da97e467f0826ae75a97006870e6654d6f6b1
cspears2002 <cspears2002@yahoo.com>
2017-06-18 20:41:41 -0700

    Finished ex11.py
    Learned about the input() function.

---
+++ ex11.py
@@ -0,0 +1,13 @@
+print("How old are you?", end=' ')
+age = input()
+print("How tall are you?", end=' ')
+height = input()
+print("How much do you weigh?", end=' ')
+weight = input()
+
+print(f"So, you're {age} years old, {height} tall and {weight} heavy.")
+
+applicant = input("Enter the applicant's name: ")
+interviewer = input("Enter the interviewer's name: ")
+time = input("Enter the appointment time: ")
+print(interviewer, "will interview", applicant, "at", time)
